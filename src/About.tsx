88a5ee135516266b8a2af40d102c89d0bdc248b6
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-02 11:33:51 -0400

    Added an about section

---
+++ src/About.tsx
@@ -0,0 +1,39 @@
+const About = () => {
+  return (
+    <div className="text-lg font-extralight w-1/2 m-auto pb-20">
+      <p>
+        I’m a proactive Computer Science graduate from LaSalle College with a
+        passion for web development. I am dedicated to developing user
+        interfaces and backend systems that work together to create a cohesive
+        user experience.
+      </p>
+      <p>
+        We did algorithms, data structures, design patterns, software
+        development life cycles, database normalization and object-oriented
+        programming to have a solid foundation in programming during my program.
+      </p>
+      <p>
+        I had the chance to work on several projects with different people
+        across several technologies improving my communication skills and
+        ability to learn fast. I also worked on a few personal projects to
+        further my discipline and perseverance.
+      </p>
+      <p>
+        I love reading books on productivity. So Good They Can’t Ignore You
+        tells you that to have a fulfilling career you need rare and valuable
+        skills. Deep Work proposes ideas to achieve such skills. Both books are
+        by Cal Newport. Atomic habits by James Clear teaches you how to build
+        good habits and break bad ones. Clean Code by Robert C. Martin
+        encourages you write to readable code. Your code should be easy to
+        understand. I recommend everyone to read the first three. Every
+        developers should also read the last one.
+      </p>
+      <p>
+        I’m keen on exploring roles in front-end or back-end development where I
+        can further my passion for creating impactful, user-friendly technology.
+      </p>
+    </div>
+  );
+};
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import  Navbar  from "./Navbar";
 import Hero from './Hero';
+import About from './About';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   )
 }
