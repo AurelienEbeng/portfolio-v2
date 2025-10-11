2a5b4042bbb0c22d6cf4ec49a6905d9f674d9ca3
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-11 11:33:20 -0400

    Worked on the project card and layout of the projects

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -3,11 +3,67 @@ const Projects = () => {
     <div>
       <div className="font-extrabold text-4xl text-white">Projects</div>
 
-      <div className="h-screen py-6">
-        <div className="border border-gray-200 w-1/4 h-full">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 p-10">
+        <div className="border border-gray-200 rounded-2xl h-130 p-4">
           <img
             src="src/assets/study_tracker_highlight.png"
             alt="study tracker image"
+            className="rounded-2xl"
+          />
+          <p>
+            MVC application with study time tracking, user authentification,
+            account management, task and session management, and analytics and
+            reporting.
+          </p>
+          <a
+            href="https://github.com/AurelienEbeng/StudyTracker"
+            target="blank"
+          >
+            <span className="tech-logo github-logo text-white"></span>
+          </a>
+        </div>
+        <div className="border border-gray-200 rounded-2xl h-130 p-4">
+          <img
+            src="src/assets/study_tracker_highlight.png"
+            alt="study tracker image"
+            className="rounded-2xl"
+          />
+          <p>
+            MVC application with study time tracking, user authentification,
+            account management, task and session management, and analytics and
+            reporting.
+          </p>
+          <a
+            href="https://github.com/AurelienEbeng/StudyTracker"
+            target="blank"
+          >
+            <span className="tech-logo github-logo text-white"></span>
+          </a>
+        </div>
+        <div className="border border-gray-200 rounded-2xl h-130 p-4">
+          <img
+            src="src/assets/study_tracker_highlight.png"
+            alt="study tracker image"
+            className="rounded-2xl"
+          />
+          <p>
+            MVC application with study time tracking, user authentification,
+            account management, task and session management, and analytics and
+            reporting.
+          </p>
+          <a
+            href="https://github.com/AurelienEbeng/StudyTracker"
+            target="blank"
+          >
+            <span className="tech-logo github-logo text-white"></span>
+          </a>
+        </div>
+        <div></div>
+        <div className="border border-gray-200 rounded-2xl h-130 p-4">
+          <img
+            src="src/assets/study_tracker_highlight.png"
+            alt="study tracker image"
+            className="rounded-2xl"
           />
           <p>
             MVC application with study time tracking, user authentification,
