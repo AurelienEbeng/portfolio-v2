de1f560502233c7d937c1994ee0a536953daed98
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-13 11:21:54 -0400

    Added a gradient overlay over a project's image

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,16 +1,19 @@
 const Projects = () => {
   return (
-    <div>
-      <div className="font-extrabold text-4xl text-white">Projects</div>
+    <div className="bg-white">
+      <div className="font-extrabold text-4xl pt-4">Projects</div>
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 p-10">
         <div className="border border-gray-200 rounded-2xl h-130 p-4">
-          <img
-            src="src/assets/study_tracker_highlight.png"
-            alt="study tracker image"
-            className="rounded-2xl"
-          />
-          <p>
+          <div className="relative">
+            <div className="w-full h-full bg-gradient-to-b from-transparent to-white absolute"></div>
+            <img
+              src="src/assets/study_tracker_highlight.png"
+              alt="study tracker image"
+              className="rounded-2xl"
+            />
+          </div>
+          <p className="text-black">
             MVC application with study time tracking, user authentification,
             account management, task and session management, and analytics and
             reporting.
