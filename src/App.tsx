7b3ca8a2939cc7494e7130acf168b2f59b207d59
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-16 10:26:29 -0400

    Moved Project.tsx to src

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import  Navbar  from "./Navbar";
 import Hero from './Hero';
 import About from './About';
-import Projects from './Projects/Projects';
+import Projects from './Projects';
 
 function App() {
 
