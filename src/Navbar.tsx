9108df44c1ee034e4eb16926a4d7412907a9a8d8
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-17 10:12:56 -0400

    Added language selector to navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const handleLanguageChange = (event: any) => {
+    setSelectedLanguage(event.target.value);
+  };
+
   return (
     <div className="flex justify-between w-full px-40 py-6 bg-[#222222] text-white fixed z-100">
-        <div className="font-extrabold">Aurelien Ebeng</div>
-        <div>
-            <ul className="flex justify-start text-base font-[Plus Jakarta Sans]">
-                <li className="px-4"><a href="#">Home</a></li>
-                <li className="px-4"><a href="#about">About</a></li>
-                <li className="px-4"><a href="#projects">Projects</a></li>
-            </ul>
-        </div>
+      <div className="font-extrabold">Aurelien Ebeng</div>
+      <div className="flex">
+        <ul className="flex justify-start text-base font-[Plus Jakarta Sans]">
+          <li className="px-4">
+            <a href="#">Home</a>
+          </li>
+          <li className="px-4">
+            <a href="#about">About</a>
+          </li>
+          <li className="px-4">
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+        <select
+          id="languageSelector"
+          value={selectedLanguage}
+          onChange={handleLanguageChange}
+        >
+          <option value="en" className="text-black">
+            English
+          </option>
+          <option value="fr" className="text-black">
+            Français
+          </option>
+        </select>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
