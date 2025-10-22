05725bb2e84ecff1a9faa9ed6f8ef3d80f43a38f
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-10-22 12:12:30 -0400

    Removed my name from the navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,40 +3,37 @@ import { languages } from "./enums/Languages";
 import { navbarDictionary } from "./language_dictionaries/NavbarDictionary";
 
 const Navbar = () => {
-  const {language, changeLanguage} = useLanguageContext();
+  const { language, changeLanguage } = useLanguageContext();
 
   const handleLanguageChange = (event: any) => {
     changeLanguage(event.target.value);
   };
 
   return (
-    <div className="flex justify-between w-full px-40 py-6 bg-[#222222] text-white fixed z-100">
-      <div className="font-extrabold">Aurelien Ebeng</div>
-      <div className="flex">
-        <ul className="flex justify-start text-base font-[Plus Jakarta Sans]">
-          <li className="px-4">
-            <a href="#">{navbarDictionary.home.get(language)}</a>
-          </li>
-          <li className="px-4">
-            <a href="#about">{navbarDictionary.about.get(language)}</a>
-          </li>
-          <li className="px-4">
-            <a href="#projects">{navbarDictionary.projects.get(language)}</a>
-          </li>
-        </ul>
-        <select
-          id="languageSelector"
-          value={language}
-          onChange={handleLanguageChange}
-        >
-          <option value={languages.en} className="text-black">
-            English
-          </option>
-          <option value={languages.fr} className="text-black">
-            Français
-          </option>
-        </select>
-      </div>
+    <div className="flex justify-end w-full px-40 py-6 bg-[#222222] text-white fixed z-100">
+      <ul className="flex justify-start text-base font-[Plus Jakarta Sans]">
+        <li className="px-4">
+          <a href="#">{navbarDictionary.home.get(language)}</a>
+        </li>
+        <li className="px-4">
+          <a href="#about">{navbarDictionary.about.get(language)}</a>
+        </li>
+        <li className="px-4">
+          <a href="#projects">{navbarDictionary.projects.get(language)}</a>
+        </li>
+      </ul>
+      <select
+        id="languageSelector"
+        value={language}
+        onChange={handleLanguageChange}
+      >
+        <option value={languages.en} className="text-black">
+          English
+        </option>
+        <option value={languages.fr} className="text-black">
+          Français
+        </option>
+      </select>
     </div>
   );
 };
