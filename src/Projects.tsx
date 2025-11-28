a49f87693006555946e67dc9ecc33c7f6ce036d7
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2025-11-27 21:17:24 -0500

    Added primal training gym project card

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,9 +1,10 @@
 import { useLanguageContext } from "./context/LanguageContext";
 import { projectsDictionary } from "./language_dictionaries/ProjectsDictionary";
-import studyTrackerImage from './assets/study-tracker-highlight.png';
-import bugTrackerImage from './assets/bug-tracker.png';
-import alphanumericImage from './assets/alphanumeric-planet-adventures.png';
-import djangoBlogImage from './assets/django-blog.png';
+import studyTrackerImage from "./assets/study-tracker-highlight.png";
+import bugTrackerImage from "./assets/bug-tracker.png";
+import alphanumericImage from "./assets/alphanumeric-planet-adventures.png";
+import djangoBlogImage from "./assets/django-blog.png";
+import primalTrainingGym from "./assets/primal-training.png";
 
 const Projects = () => {
   const { language } = useLanguageContext();
@@ -14,6 +15,35 @@ const Projects = () => {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
+        <div className="projectCard">
+          <div className="relative">
+            <div className="w-full h-full bg-gradient-to-b from-transparent to-white absolute"></div>
+            <img
+              src={primalTrainingGym}
+              alt="primal training gym image"
+              className="rounded-2xl"
+            />
+          </div>
+          <div className="text-4xl font-bold mt-2">Primal Training Gym</div>
+          <p className="text-black text-lg text-left">
+            Primal training gym website. Still working on it.
+          </p>
+          <div className="flex justify-center items-center pt-20">
+            <a
+              href="https://performancetraininggym.netlify.app/"
+              target="blank"
+              className="py-2 px-4 rounded-2xl bg-gray-500 font-bold text-xl"
+            >
+              Live
+            </a>
+            <a
+              href="https://github.com/AurelienEbeng/performance-training-gym"
+              target="blank"
+            >
+              <span className="tech-logo github-logo text-black"></span>
+            </a>
+          </div>
+        </div>
         <div className="projectCard">
           <div className="relative">
             <div className="w-full h-full bg-gradient-to-b from-transparent to-white absolute"></div>
@@ -81,7 +111,6 @@ const Projects = () => {
             <span className="tech-logo github-logo text-black"></span>
           </a>
         </div>
-        <div className="hidden lg:block"></div>
         <div className="projectCard">
           <div className="relative">
             <div className="w-full h-full bg-gradient-to-b from-transparent to-white absolute"></div>
