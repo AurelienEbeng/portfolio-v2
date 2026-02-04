6544b58d82005b92d65ac3773732419567779379
Aurelien Ebeng <100911954+AurelienEbeng@users.noreply.github.com>
2026-02-04 11:25:43 -0500

    Removed logos that were not frontend related

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -32,30 +32,9 @@ const Hero = () => {
           <span className="tech-logo react-js-logo"></span>
           <span className="tech-logo typescript-logo"></span>
           <span className="tech-logo tailwindcss-logo"></span>
-          <span className="tech-logo material-ui-logo"></span>
-          <span className="tech-logo sass-logo"></span>
-          <span className="tech-logo bootstrap-logo"></span>
           <span className="tech-logo figma-logo"></span>
-        </div>
-        <div className="flex justify-evenly mt-2">
-          <span className="tech-logo spring-logo"></span>
-          <span className="tech-logo dotnet-logo"></span>
-          <span className="tech-logo django-logo"></span>
-          <span className="tech-logo php-logo"></span>
-        </div>
-        <div className="flex justify-evenly mt-2">
-          <span className="tech-logo mysql-logo"></span>
-          <span className="tech-logo microsoftsqlserver-logo"></span>
-          <span className="tech-logo sqlite-logo"></span>
-          <span className="tech-logo oracle-logo"></span>
-          <span className="tech-logo firebase-logo"></span>
-        </div>
-        <div className="flex justify-evenly mt-2">
           <span className="tech-logo git-logo"></span>
           <span className="tech-logo github-logo"></span>
-          <span className="tech-logo docker-logo"></span>
-          <span className="tech-logo wordpress-logo"></span>
-          <span className="tech-logo graphql-logo"></span>
         </div>
       </div>
     </div>
